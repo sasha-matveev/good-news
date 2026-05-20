18102c400b9dd411987fb1888ac7b296b82e02fe
alexander matveev <dev.alexander.matveev@gmail.com>
2026-05-21 01:43:56 +0200

    Fix default feed sorting by date

--- frontend/src/test/index-html.test.ts
+++ frontend/src/test/index-html.test.ts
@@ -10,3 +10,11 @@ test("frontend index.html mounts the app entrypoint", async () => {
   expect(html).toContain('<div id="root"></div>');
   expect(html).toContain('<script type="module" src="/src/main.tsx"></script>');
 });
+
+test("frontend index.html links the favicon asset", async () => {
+  const html = await readFile(new URL("../../index.html", import.meta.url), "utf8");
+  const favicon = await readFile(new URL("../../public/favicon.svg", import.meta.url), "utf8");
+
+  expect(html).toContain('<link rel="icon" type="image/svg+xml" href="/favicon.svg" />');
+  expect(favicon).toContain('viewBox="0 0 64 64"');
+});
